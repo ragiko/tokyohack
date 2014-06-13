d20226029283728f1252779eb8c9f7c2376f1554
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-06-13 16:44:15 +0900

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tokyohack::Application.config.secret_key_base = '01bc2aae18bd5b9e1b8a9c8b744c7a032c4d6b67c1face89638eadbd088e6a6a017b18690b69fd1f5a8afb62bf525561dcd85045c228d358827e354b6804a339'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tokyohack::Application.config.session_store :cookie_store, key: '_tokyohack_session'
